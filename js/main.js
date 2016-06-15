74af0190e21b071049999d1c0d561de386037cac
lazybear99 <abrarahmed377@hotmail.com>
2016-06-15 19:09:57 +0600

    complete blog section

--- js/main.js
+++ js/main.js
@@ -1,14 +1,54 @@
 //object contain header dom elements and functions for animation
-let height = $(window).height()
-$(".home").css({
-  'height' : height,
-})
-let arr = []
-let li = $("nav").find('li')
-for (let i = 0; i<li.length; i++ ) {
-  arr.push(li[i].firstChild.nodeValue)
+const home = {
+  // caching the dom
+  elem () {
+    this.$home = $(".home")
+    this.height = $(window).height()
+    this.$menu = this.$home.find('.menu-icon')
+  },
+  // set home section height as full window height
+  setwindowHeight () {
+    this.$home.css({
+      'height' : this.height,
+    })
+  },
+  // menu icon animation
+
+  // initialize all methods
+  init () {
+    this.elem()
+    this.setwindowHeight()
+
+  },
+  // rendering all methods
+  render () {
+    this.init()
+  }
 }
 
-for(let j =0; j<arr.length; j++) {
-  console.log(`<li>${arr[j]}</li>`)
-}
+$(document).ready(function () {
+  home.render()
+//   $.jvmobilemenu({
+//   mainContent: $('.page'),
+//   theMenu: $('.mobile-nav'),
+//   slideSpeed: 0.3,
+//   menuWidth: 240,
+//   position: 'right',
+//   menuPadding: '20px 20px 60px'
+// });
+
+$('body').jvmobilemenu({
+
+// selector to click outside menu to close
+notMenuClick: $('.page'),
+
+// speed to open and close menu
+slideSpeed: 0.3,
+
+// width of the mobile menu
+menuWidth: 240,
+position: 'right',
+menuPadding: '20px 20px 60px'
+
+});
+});
